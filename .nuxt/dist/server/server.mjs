e27e537aed42ca00fdcf40e5fd253079e64b87b4
Evan Zaqli <evanzaqli1@gmail.com>
2024-06-27 17:08:02 +0700

    pp

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///D:/Responsi/Responsi_Pemograman_Web/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script>
+import HeroSection from '../components/HeroSection'
+import AboutSection from '~/components/AboutSection.vue';
+import ProjectsSection from '~/components/ProjectsSection.vue';
+import ContactSection from '~/components/ContactSection.vue';
+
+</script>./Index.vue
+
+
+<template>
+  <div>
+    <HeroSection />
+  </div>
+</template>
+
+export default {
+  components: {
+    HeroSection,
+  }
+}
+
